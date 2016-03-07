69f8755b86a3011f9ebfd4448680fc77b9acd138
Jasper <jazzbeenjamin@me.com>
2016-03-07 16:35:41 -0600

    Comment out more code for minor release

--- com/nekrosius/drizzardwars/managers/CommandManager.java
+++ com/nekrosius/drizzardwars/managers/CommandManager.java
@@ -13,7 +13,7 @@ public class CommandManager {
 		pl.getCommand("party").setExecutor(new PartyCommand(pl));
 		pl.getCommand("points").setExecutor(new PointsCommand(pl));
 		pl.getCommand("help").setExecutor(new HelpCommand());
-		pl.getCommand("ability").setExecutor(new AbilityCommand(pl));
+//		pl.getCommand("ability").setExecutor(new AbilityCommand(pl));
 	}
 
 	public Main getMainClass() {
